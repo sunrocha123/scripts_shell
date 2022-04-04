f6a5b9f8b85ef2e8f5a5df548586355e47b5e110
sunrocha123 <judson.santana_123@outlook.com>
2022-04-04 12:18:07 -0300

    Adicionado loop para que o usuário tenha a possibilidade de calcular a nota de outros alunos

--- calculo_notas.sh
+++ calculo_notas.sh
@@ -4,27 +4,38 @@ echo '========================'
 echo 'CÁLCULO DE MÉDIAS'
 echo '========================'
 
-read -p 'Digite seu nome: ' nome_aluno
-
 echo 'Olá' $nome_aluno '! Bem-vindo ao nosso app!!'
 
-read -p 'Digite a nota do primeiro bimestre: ' primeiro_bimestre
-read -p 'Digite a nota do segundo bimestre: ' segundo_bimestre
-read -p 'Digite a nota do terceiro bimestre: ' terceiro_bimestre
-read -p 'Digite a nota do quarto bimestre: ' quarto_bimestre
+seguir_com_app=1
 
-soma_notas=$(expr $primeiro_bimestre + $segundo_bimestre + $terceiro_bimestre + $quarto_bimestre)
-#echo 'Soma das notas:'$soma_notas
-media_final=$(expr $soma_notas / 4)
-#echo 'Cálculo final da média:'$media_final
+while [ $seguir_com_app -eq 1 ];do
+	read -p 'Digite seu nome: ' nome_aluno 
 
-if [ $media_final -lt 5 ];then
-	echo 'Aluno reprovado!'
-	echo 'Nota final:' $media_final
-elif [ $media_final -ge 5 -a $media_final -le 6 ]; then
-	echo 'Aluno de recuperação!'
-	echo 'Nota até o momento:' $media_final
-else
-	echo 'Aluno aprovado!'
-	echo 'Nota final:' $media_final
-fi
+	read -p 'Digite a nota do primeiro bimestre: ' primeiro_bimestre
+	read -p 'Digite a nota do segundo bimestre: ' segundo_bimestre
+	read -p 'Digite a nota do terceiro bimestre: ' terceiro_bimestre
+	read -p 'Digite a nota do quarto bimestre: ' quarto_bimestre
+
+	soma_notas=$(expr $primeiro_bimestre + $segundo_bimestre + $terceiro_bimestre + $quarto_bimestre)
+	#echo 'Soma das notas:'$soma_notas
+	media_final=$(expr $soma_notas / 4)
+	#echo 'Cálculo final da média:'$media_final
+
+	if [ $media_final -lt 5 ];then
+		echo 'Aluno reprovado!'
+		echo 'Nota final:' $media_final
+	elif [ $media_final -ge 5 -a $media_final -le 6 ]; then
+		echo 'Aluno de recuperação!'
+		echo 'Nota até o momento:' $media_final
+	else
+		echo 'Aluno aprovado!'
+		echo 'Nota final:' $media_final
+	fi
+
+	read -p 'Deseja calcular a média de outro aluno?(s/n) ' resposta
+
+	if [ $resposta = 'n' ]; then
+		seguir_com_app=0
+		echo 'Obrigado por usar nosso aplicativo!'
+	fi
+done
